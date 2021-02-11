79776b7c7376e2154fcab5d6f96c7e2db4312ddd
bkf2020 <74793713+bkf2020@users.noreply.github.com>
2021-02-11 15:59:18 -0800

    day 11 code

---
+++ 2020/Day11/day11part2.cpp
@@ -0,0 +1,72 @@
+#include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
+
+vector<vector<char>> prev_seating;
+vector<vector<char>> next_seating;
+
+// 8 directions
+int dx[8] = {-1, 0, 1, 0, 1, -1, 1, -1};
+int dy[8] = {0, -1, 0, 1, 1, -1, -1, 1};
+bool first_round = true;
+
+bool in_range(int i, int j) {	
+	return (i >= 0) && (j >= 0) && (i < prev_seating.size()) && (j < prev_seating[0].size());
+}
+
+bool changed() {
+	return prev_seating != next_seating;
+}
+
+void simulate() {
+	prev_seating = next_seating;
+	for(int i = 0; i < prev_seating.size(); i++) {
+		for(int j = 0; j < prev_seating[i].size(); j++) {
+			int occupied_count = 0;
+			for(int k = 0; k < 8; k++) {
+				// check all 8 adjacent
+				int new_i = i + dx[k];
+				int new_j = j + dy[k];
+				while(in_range(new_i, new_j)) {
+					if(prev_seating[new_i][new_j] == '#') {
+						occupied_count++;
+						break;
+					}
+					else if(prev_seating[new_i][new_j] == 'L') {
+						break;
+					}
+					new_i += dx[k];
+					new_j += dy[k];
+				}
+			}
+			if(occupied_count == 0 && prev_seating[i][j] == 'L') next_seating[i][j] = '#';
+			if(occupied_count >= 5 && prev_seating[i][j] == '#') next_seating[i][j] = 'L';
+		}
+	}
+}
+
+int count_occupied() {
+	int count = 0;
+	for(vector<char> vc : next_seating) { // final seating at this point
+		for(char c : vc) {
+			if(c == '#') count++;
+		}
+	}
+	return count;
+}
+
+int main() {
+	string line;
+	while(cin >> line) {
+		vector<char> curr_line;
+		for(char c : line) {
+			curr_line.push_back(c);
+		}
+		next_seating.push_back(curr_line);
+	}
+	while(changed()) {
+		simulate();
+	}
+	cout << count_occupied() << '\n';
+}
