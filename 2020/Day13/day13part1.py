13a73a21065fbb2116dcdad14758a9e7d54045ec
bkf2020 <74793713+bkf2020@users.noreply.github.com>
2021-02-13 17:45:59 -0800

    Update day13part1.py

--- 2020/Day13/day13part1.py
+++ 2020/Day13/day13part1.py
@@ -7,14 +7,14 @@ min_waiting = 2147483647
 min_id = 0
 
 for b in buses_str:
-  if b != x:
-    bus_id = int(b)
-    closest = bus_id * ((wanted_time + bus_id - 1) // (bus_id))
-    # (wanted_time + bus_id - 1) // (bus_id) gives the ceiling
-    waiting_time = closest - wanted_time
-    
-    if waiting_time < min_waiting
-      min_waiting = waiting_time
-      min_id = bus_id
+	if b != 'x':
+		bus_id = int(b)
+    	closest = bus_id * ((wanted_time + bus_id - 1) // (bus_id))
+    	# (wanted_time + bus_id - 1) // (bus_id) gives the ceiling
+    	waiting_time = closest - wanted_time
 
+    	if waiting_time < min_waiting:
+      		min_waiting = waiting_time
+      		min_id = bus_id
+		
 print(min_waiting * min_id)
