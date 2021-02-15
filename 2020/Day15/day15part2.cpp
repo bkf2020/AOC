33370deec5366fa481cabf6e9d0107dd08ca299a
bkf2020 <74793713+bkf2020@users.noreply.github.com>
2021-02-15 15:18:52 -0800

    day 15 optimization inspired by mebeim's aoc

--- 2020/Day15/day15part2.cpp
+++ 2020/Day15/day15part2.cpp
@@ -1,16 +1,12 @@
 #include <iostream>
-#include <map>
 #include <vector>
 #include <string>
 #include <sstream>
 using namespace std;
 
-struct location {
-	int recent, previous, occupied = 0;
-};
+int previous[30000000];
 
 int main() {
-	map<int, location> locations;
 	vector<int> starting;
 	string line; getline(cin, line);
 	stringstream ss(line);
@@ -20,35 +16,22 @@ int main() {
 	while(getline(ss, part, ',')) {
 		starting.push_back(stoi(part));
 	}
-	for(int i = 0; i < starting.size(); i++) {
-		locations[starting[i]].recent = i;
-		locations[starting[i]].occupied = 1;
+	// last one should not be ignored...
+	for(int i = 0; i < starting.size() - 1; i++) {
+		previous[starting[i]] = i + 1;
+		// we increase by one, because if i is zero, the checks in the next loop won't work
 	}
 	int prev = starting.back();
 	for(int i = starting.size(); i < 30000000; i++) {
-		if(locations[prev].occupied == 1) {
+		if(!previous[prev]) { // no previous number
+			previous[prev] = i; // recent turn
 			prev = 0;
-			if(locations[0].occupied == 0) {
-				locations[0].recent = i;
-			}
-			else {
-				locations[0].previous = locations[0].recent;
-				locations[0].recent = i;
-			}
-			locations[0].occupied++;
 		}
 		else {
-			int previous_turn = locations[prev].recent;
-			int turn_before = locations[prev].previous;
-			prev = previous_turn - turn_before;
-			if(locations[prev].occupied == 0) {
-				locations[prev].recent = i;
-			}
-			else {
-				locations[prev].previous = locations[prev].recent;
-				locations[prev].recent = i;
-			}
-			locations[prev].occupied++;
+			int recent_turn = i;
+			int turn_before = previous[prev];
+			previous[prev] = i;
+			prev = recent_turn - turn_before;
 		}
 	}
 	cout << prev << '\n';
